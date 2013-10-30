32e87e516284d53b80eecc78a2a1ec42c081aefd
bubylou <bubylou7@gmail.com>
2013-10-30 19:15:06 -0400

    new homepage

---
+++ js/hotkeys.js
@@ -0,0 +1,9 @@
+shortcut.add("1",function() {document.location.href = hotlink1; });
+shortcut.add("2",function() {document.location.href = hotlink2; });
+shortcut.add("3",function() {document.location.href = hotlink3; });
+shortcut.add("4",function() {document.location.href = hotlink4; });
+shortcut.add("5",function() {document.location.href = hotlink5; });
+shortcut.add("6",function() {document.location.href = hotlink6; });
+shortcut.add("7",function() {document.location.href = hotlink7; });
+shortcut.add("8",function() {document.location.href = hotlink8; });
+shortcut.add("9",function() {document.location.href = hotlink9; });
